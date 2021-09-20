4aca023aa2952e35e37b485c1b3168cb74045fe1
Dominik Duda <gospodorz@gmail.com>
2021-09-20 20:31:08 +0200

    Fix stuck initial fav

--- generate_stooq.rb
+++ generate_stooq.rb
@@ -101,7 +101,7 @@ output << <<~PAGE_TOP
             <head>
             <script>
             window.watcherRefreshMenu = function() {
-              if (!window.watcherGetCookie('marked_charts')) {
+              if (!window.watcherGetCookie('marked_charts') == null) {
                 return
               }
               const els = document.getElementsByClassName('chart-link')
@@ -258,8 +258,8 @@ function setCookie(name,value,days = 9999) {
   color: #ffbf00;
 }
 
-.marked {
-  color: #009cff !important;
+.divider-link .marked {
+  color: #009cff;
 }
 
 .darkMode > .divider {
